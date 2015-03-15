c8c29298f4b316bf187c707864e6138b0f8b68f8
alexlehm <alexlehm@gmail.com>
2015-03-15 18:43:55 +0100

    unit issue example

---
+++ src/test/java/cx/lehmann/vertx/unit/UnitTestExample.java
@@ -0,0 +1,32 @@
+package cx.lehmann.vertx.unit;
+
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.impl.LoggerFactory;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/*
+ * smtp client test using vertx unit
+ */
+/**
+ * @author <a href="http://oss.lehmann.cx/">Alexander Lehmann</a>
+ *
+ */
+@RunWith(VertxUnitRunner.class)
+public class UnitTestExample {
+
+  private static final Logger log = LoggerFactory.getLogger(UnitTestExample.class);
+
+  @Test
+  public void testMail(TestContext context) throws Exception {
+    log.info("starting test");
+    Async async = context.async();
+
+    throw new Exception(); 
+  }
+
+}
